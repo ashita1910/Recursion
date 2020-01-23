8008c01867e9cda017c85fc5201283cbe6497bbb
ashitag64 <ashita.goyal_cs18@gla.ac.in>
2020-01-22 20:10:36 -0800

    add PrimeNumberCalculator.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,22 +8,28 @@
 package main;
 
 import recursion.FactorialCalculator;
+import recursion.PowerCalculator;
 import recursion.TowerOfHanoiSolver;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*FactorialCalculator calculator = new FactorialCalculator();
         final Scanner scanner = new Scanner(System.in);
+        /*FactorialCalculator calculator = new FactorialCalculator();
         System.out.println("Enter the number you want to find the factorial for: ");
         int number = scanner.nextInt();
         int calculatedFactorial = calculator.calculateFactorial(number);
-        System.out.println("The factorial of " + number + " is : " + calculatedFactorial);*/
-        final Scanner scanner = new Scanner(System.in);
+        System.out.println("The factorial of " + number + " is : " + calculatedFactorial);
         System.out.println("Enter the no of disks:");
         int numberOfDisks = scanner.nextInt();
         TowerOfHanoiSolver towerOfHanoiSolver = new TowerOfHanoiSolver();
-        towerOfHanoiSolver.hanoi(numberOfDisks, 'A', 'B', 'C');
+        towerOfHanoiSolver.hanoi(numberOfDisks, 'A', 'B', 'C');*/
+        System.out.println("Enter the number:");
+        int number = scanner.nextInt();
+        System.out.println("Enter the power:");
+        int power = scanner.nextInt();
+        PowerCalculator powerCalculator = new PowerCalculator();
+        System.out.println("Power of number is:" + powerCalculator.powerCal(number, power));
     }
 }
--- src/recursion/PowerCalculator.java
+++ src/recursion/PowerCalculator.java
@@ -1,4 +1,9 @@
 package recursion;
 
 public class PowerCalculator {
+    public int powerCal(int number, int power){
+        if(power == 1)
+            return number;
+        return number * powerCal(number, power - 1);
+    }
 }
